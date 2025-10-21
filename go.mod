59ab96099b6868a412bec98c429eed3e21ed1d49
jadereyn <jadereyn@j.r>
2025-10-20 19:06:32 -0700

    xor distance function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module cs249-dht
 
 go 1.24.3
+
+require github.com/go-faster/xor v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
 import "fmt"
+import "github.com/go-faster/xor"
+
+const NODE_ID_BUFFER_SIZE int = 20 // 20 bytes in 160-bit node ID
 
 func main() {
-    fmt.Println("Hello, World!")
+    x := make([]byte, NODE_ID_BUFFER_SIZE, NODE_ID_BUFFER_SIZE)
+    y := make([]byte, NODE_ID_BUFFER_SIZE, NODE_ID_BUFFER_SIZE)
+
+    // Initializing the elements
+    for i := 0; i < 10; i++ {
+        x[i] = byte(i + 1)
+        y[i] = byte(i + 1)
+    }
+
+    res := xor_distance(x, y)
+    fmt.Println(res)
+}
+
+// assuming network byte order (big endian)
+func xor_distance(x, y []byte) []byte {
+    fmt.Println(x)
+    fmt.Println(y)
+
+    res := make([]byte, 20, 20)
+    xor.Bytes(res, x, y)
+    return res
 }
